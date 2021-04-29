15e1990dab3315570e0a65d86897b28f3d0f4e40
Pez Cuckow <email@pezcuckow.com>
2021-04-29 13:45:23 +0200

    ♻️ Redesign to work as a single run script

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import WebSocket from "ws";
 import axios from "axios";
 
-const ws = new WebSocket(
-  "wss://pusher.workadventu.re/admin/rooms?token=zpxngfjebcoehe4f5eke&roomId=@/harvest/harvest/virtualoffice"
-);
-
 const members = {};
+
+const WAIT_BEFORE_UPDATING = 1 * 1000; // ms
+
+if (!process.env.SLACK_CHANNEL_ID) {
+  console.log("Must specify SLACK_CHANNEL_ID in .env");
+  process.exit();
+}
+
+if (!process.env.SLACK_ACCESS_TOKEN) {
+  console.log("Must specify SLACK_ACCESS_TOKEN in .env");
+  process.exit();
+}
+
+if (!process.env.SLACK_ACCESS_TOKEN) {
+  console.log("Must specify work adventure WEBSOCKET_URL in .env");
+  process.exit();
+}
 
 const countOnlineUsers = () =>
   Object.entries(members).filter(([name, status]) => status).length;
@@ -36,17 +52,38 @@ async function joinSlackChannel() {
       },
     }
   );
+
+  if (data.ok) {
+    console.log("Joined slack channel");
+  } else {
+    console.error("Could not join slack channel", data);
+  }
 }
 
-let firstUpdate = true;
-async function updateSlack() {
-  firstUpdate = false
+async function getSlackChannelInfo() {
+  const { data } = await axios.get(`https://slack.com/api/conversations.info`, {
+    params: {
+      channel: process.env.SLACK_CHANNEL_ID,
+    },
+    headers: {
+      Authorization: `Bearer ${process.env.SLACK_ACCESS_TOKEN}`,
+    },
+  });
 
+  if (!data.ok) {
+    return console.error("Could not load channel info", data);
+  }
+
+  console.log("Loaded channel info");
+  return data.channel;
+}
+
+async function updateSlackTopic(newTopic) {
   const { data } = await axios.post(
     `https://slack.com/api/conversations.setTopic`,
     {
       channel: process.env.SLACK_CHANNEL_ID,
-      topic: `Harvest’s Virtual Office - ${countOnlineUsers()} online!`,
+      topic: newTopic,
     },
     {
       headers: {
@@ -56,23 +93,37 @@ async function updateSlack() {
   );
 
   if (data.ok) {
-    console.log("Channel topic updated");
+    console.log(`Channel topic updated to ${newTopic}`);
   } else {
     console.error("Channel topic NOT updated", data);
   }
 }
 
-const updateSlackSoon = debounce(updateSlack, 1000 * 15, false);
-const updateSlackLater = debounce(updateSlack, 1000 * 60 * 5, false);
+async function updateSlack() {
+  const userCount = countOnlineUsers();
+  const newTopic = `Harvest’s Virtual Office - ${countOnlineUsers()} online!`;
+  const existingTopic = (await getSlackChannelInfo())?.topic.value;
+
+  if (existingTopic == newTopic) {
+    console.log(`No update needed, topic is already: ${existingTopic}`);
+    process.exit(0);
+  }
+
+  await updateSlackTopic(newTopic);
+
+  process.exit();
+}
+
+const updateSlackSoon = debounce(updateSlack, WAIT_BEFORE_UPDATING, false);
 
 function connectToWS() {
-  ws.on("open", function open(data) {
-    if (!data) {
-      return;
-    }
-    data = JSON.parse(data);
+  const ws = new WebSocket(process.env.WEBSOCKET_URL);
 
-    console.log("Open", data);
+  ws.on("open", function open(data) {
+    console.log("Connected to WS");
+
+    // This happens if there are no users on the server == no messages
+    updateSlackSoon();
   });
 
   ws.on("message", function incoming(data) {
@@ -81,25 +132,28 @@ function connectToWS() {
     }
     data = JSON.parse(data);
 
-    if (!data.data.name) {
-      return console.log("Name missing", data.data);
+    if (!data.data.uuid) {
+      return console.log("UUID missing", data.uuid);
     }
 
     if (data.type === "MemberJoin") {
-      members[data.data.name] = true;
-      console.log(`${data.data.name} joined`);
+      members[data.data.uuid] = true;
+      console.log(`${data.data.uuid} ${data.data.name} joined`);
     } else if (data.type === "MemberLeave") {
-      members[data.data.name] = false;
-      console.log(`${data.data.name} left`);
+      members[data.data.uuid] = false;
+      console.log(`${data.data.uuid} left`);
     }
 
-    if (firstUpdate) {
-      updateSlackSoon()
-    } else {
-      updateSlackLater()
-    }
+    // After the first post, wait longer between messages
+    updateSlackSoon();
+  });
+
+  ws.on("close", function () {
+    console.error("Disconnected from WS");
+    process.exit();
   });
 }
 
 joinSlackChannel();
 connectToWS();
+setTimeout(() => process.exit(), 60 * 1000);
